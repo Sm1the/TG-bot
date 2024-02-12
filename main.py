1e689f35417819e62b560da47a67d2e4e379a58b
Sm1the <apxahreji123@gmail.com>
2024-02-13 05:56:12 +0700

    fix methods

--- main.py
+++ main.py
@@ -5,54 +5,79 @@ from telebot import types
 import Path
 import DBHandler
 
+#Show DB List
+#Close bot
+#Restart bot
 
+
+
+#Чтение ключа бота из json файла
 with open('token_api.json', 'r') as token_api:
     bot_token = json.load(token_api)
-    
+#Чтение всего текста бота из json файла  
 with open('text_message.json', 'r', encoding='utf-8') as text_message:
     bot_message = json.load(text_message)
-
+#Реализация соединения бота по ключу
 bot = telebot.TeleBot(bot_token['token_api'])
 
 
-
+#Метод стартового меню и входа в общение с ботом
 @bot.message_handler(commands=['start'])
 def startBot(message):
-  markup = types.InlineKeyboardMarkup()
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
   first_message = f"<b>{message.from_user.first_name}</b>, <b>{bot_message['start_message']}</b>"
-  button_menu_1 = types.InlineKeyboardButton(text = bot_message['menu_text_1'], callback_data='select_1')
+  button_menu_1 = types.KeyboardButton(text = bot_message['menu_text_1'])
   markup.row(button_menu_1)
-  button_menu_2 = types.InlineKeyboardButton(text = bot_message['menu_text_2'], callback_data='select_2')
+  button_menu_2 = types.KeyboardButton(text = bot_message['menu_text_2'])
   markup.row(button_menu_2)
-  button_menu_3 = types.InlineKeyboardButton(text = bot_message['menu_text_3'], callback_data='select_3')
+  button_menu_3 = types.KeyboardButton(text = bot_message['menu_text_3'])
   markup.row(button_menu_3)
   bot.send_message(message.chat.id, first_message, parse_mode='html', reply_markup=markup)
+  bot.register_next_step_handler(message, on_click_menu)
   
-@bot.callback_query_handler(func=lambda call:True)
-def response(function_call):
-  if function_call.message:
-     markup = types.InlineKeyboardMarkup()
-     menu_message = ""
-     if function_call.data == "select_1":
-        menu_message = f"{bot_message['bullying_info']}"
-        markup.add(types.InlineKeyboardButton(text = bot_message['menu_text_1'], callback_data='select_1'),types.InlineKeyboardButton(text = bot_message['menu_text_3'], callback_data='select_3'))
-        button_menu_1 = types.InlineKeyboardButton(text = bot_message['menu_text_1'])
-        markup.add(button_menu_1)
-        button_menu_2 = types.InlineKeyboardButton(text = bot_message['menu_text_3'])
-        markup.add(button_menu_2)
-        button_menu_3 = types.InlineKeyboardButton("Перейти на сайт", url="https://ru.wikipedia.org/wiki/%D0%A2%D1%80%D0%B0%D0%B2%D0%BB%D1%8F")
-        markup.add(button_menu_3)
-        bot.send_message(function_call.message.chat.id, menu_message, reply_markup=markup)
-        bot.answer_callback_query(function_call.id)
-     #git push -u origin mainif function_call.data == "select_2":
 
-     if function_call.data == "select_3":
-        menu_message = f"{bot_message['exit_message']}"
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.add(types.KeyboardButton(text = bot_message['exit_message_callback']))
-        bot.send_message(function_call.message.chat.id, menu_message, reply_markup=markup)
-        bot.answer_callback_query(function_call.id)
+def on_click_menu(message):
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  menu_message = ""
+  if message.text == bot_message['menu_text_1']:
+    menu_message = f"{bot_message['bullying_info']}"
+    markup.add(types.KeyboardButton(text = bot_message['menu_text_2']),types.KeyboardButton(text = bot_message['menu_text_3']))
+    bot.register_next_step_handler(message, on_click_second_menu)
+    
+  if message.text == bot_message['menu_text_2']:
+      menu_message = f"{bot_message['copy_info']}"
+      markup.add(types.KeyboardButton(text = 'Да'),types.KeyboardButton(text = 'Нет'))
+      bot.register_next_step_handler(message, create_application)
+    
+  if message.text == bot_message['menu_text_3']:
+      menu_message = f"{bot_message['exit_message']}"
+      markup.add(types.KeyboardButton(text = bot_message['exit_message_callback']))
 
+    
+  bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+  
+
+def on_click_second_menu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    menu_message = ""
+    if message.text == bot_message['menu_text_2']:
+      menu_message = f"{bot_message['copy_info']}"
+      markup.add(types.KeyboardButton(text = 'Да'),types.KeyboardButton(text = 'Нет'))
+      bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+      bot.register_next_step_handler(message, create_application)
+    if message.text == bot_message['menu_text_3']:
+      exit_bot(message)
+
+
+#Метод закрытия диалога с ботом      
+def exit_bot(message):
+      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+      menu_message = f"{bot_message['exit_message']}"
+      markup.add(types.KeyboardButton(text = bot_message['exit_message_callback']))
+      bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+    
+
+    
      
-bot.infinity_polling()
+bot.polling(non_stop=True)
 
