d71855352eededf88668aa4767f50f3025d5f0dd
Sm1thZe <apxahreji123@gmail.com>
2024-02-13 20:30:17 +0700

    fix

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import json
 import telebot
 from telebot import types
 
-import Path
-import DBHandler
 
 #Show DB List
 #Close bot
@@ -17,6 +15,9 @@ with open('token_api.json', 'r') as token_api:
 #Чтение всего текста бота из json файла  
 with open('text_message.json', 'r', encoding='utf-8') as text_message:
     bot_message = json.load(text_message)
+#
+with open('file_path.json', 'r', encoding='utf-8') as file_path:
+    bot_file_path = json.load(file_path)
 #Реализация соединения бота по ключу
 bot = telebot.TeleBot(bot_token['token_api'])
 
@@ -38,7 +39,7 @@ bulllying_counter = ""
 @bot.message_handler(commands=['start'])
 def startBot(message):
   markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-  first_message = f"<b>{message.from_user.first_name}</b>, <b>{bot_message['start_message']}</b>"
+  first_message = f"<b>{message.from_user.first_name}</b><b>{bot_message['start_message']}</b>"
   button_menu_1 = types.KeyboardButton(text = bot_message['menu_text_1'])
   markup.row(button_menu_1)
   button_menu_2 = types.KeyboardButton(text = bot_message['menu_text_2'])
@@ -138,7 +139,83 @@ def get_user_bullying(message):
     menu_message = f"{bot_message['user_bullying']}"
     markup.add(types.KeyboardButton(bot_message['yes']),types.KeyboardButton(bot_message['no']))
     bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+    bot.register_next_step_handler(message, get_user_violence)
    
-     
+def get_user_violence(message):
+    if message.text == bot_message['yes']:
+      user_bullying = bot_message['yes']
+      print(user_bullying)
+    if message.text == bot_message['no']:
+      user_bullying = bot_message['no']
+      print(user_bullying)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    menu_message = f"{bot_message['user_violence']}"
+    markup.add(types.KeyboardButton(bot_message['yes']),types.KeyboardButton(bot_message['no']))
+    bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+    bot.register_next_step_handler(message, get_user_sexual_violence)
+      
+def get_user_sexual_violence(message):
+    if message.text == bot_message['yes']:
+      user_violence = bot_message['yes']
+      print(user_violence)
+    if message.text == bot_message['no']:
+      user_violence = bot_message['no']
+      print(user_violence)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    menu_message = f"{bot_message['user_sexual_violence']}"
+    markup.add(types.KeyboardButton(bot_message['yes']),types.KeyboardButton(bot_message['no']))
+    bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+    bot.register_next_step_handler(message, get_user_bulllying_counter)
+    
+def get_user_bulllying_counter(message):
+    if message.text == bot_message['yes']:
+      user_sexual_violence = bot_message['yes']
+      print(user_sexual_violence)
+    if message.text == bot_message['no']:
+      user_sexual_violence = bot_message['no']
+      print(user_sexual_violence)
+    menu_message = f"{bot_message['bulllying_counter']}"
+    bot.send_message(message.from_user.id, menu_message, parse_mode='html', reply_markup=clear_unber_buttons) 
+    bot.register_next_step_handler(message, get_pull_user_info)
+
+def get_pull_user_info(message):
+    bulllying_counter = message.text
+    print(bulllying_counter)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    menu_message = f"{bot_message['pull_user_info']}"
+    markup.add(types.KeyboardButton(bot_message['upload_file']),types.KeyboardButton(bot_message['without_file']))
+    bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+    bot.register_next_step_handler(message, select_user_send_file)
+
+def select_user_send_file(message):
+    if message.text == bot_message['upload_file']:
+      handle_docs_photo(message)
+    if message.text == bot_message['without_file']:
+      close_user_request(message)
+
+
+@bot.message_handler(content_types=['photo', 'video'])
+def handle_docs_photo(message):
+
+    try:
+
+
+        file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        src='C:/Users/apxah/Desktop/1/TG-bot/files/'+file_info.file_path
+        with open(src, 'wb') as new_file:
+           new_file.write(downloaded_file)
+        bot.reply_to(message,"Фото добавлено") 
+
+    except Exception as e:
+        bot.reply_to(message,e )
+    
+def close_user_request(message):
+      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+      menu_message = f"{bot_message['close_user_request']}"
+      markup.add(types.KeyboardButton(text = bot_message['exit_message_callback']))
+      bot.send_message(message.chat.id, menu_message, parse_mode='html', reply_markup=markup)
+
 bot.polling(non_stop=True)
 
